27052eb4298c156891420db9d6989cc031bbbe73
unknown <matheus.duartesilva42@gmail.com>
2023-12-12 20:42:22 -0300

    implementação das rotas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 const fastify = require("fastify");
+const BookingRepository = require("./bookings/BookingRepository");
+const BookingService = require("./bookings/BookingService");
 
 const app = fastify({ logger: true })
+
+const bookingRepository = new BookingRepository()
+const bookingService = new BookingService(bookingRepository)
+
+app.post('/api/bookings', (req, reply) => {
+    const { roomId, guestName, checkInDate, checkOutDate } = req.body
+
+    const booking = bookingService({ roomId, guestName, checkInDate, checkOutDate })
+
+    reply.code(201).send({ message: "Booking created successfully", booking })
+})
 
 module.exports = app
