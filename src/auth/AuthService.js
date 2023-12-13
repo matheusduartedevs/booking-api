fdd8eb75f0e55ed860350fe89f6b804f52080a3f
unknown <matheus.duartesilva42@gmail.com>
2023-12-13 16:59:47 -0300

    implementação do bcrypte e jwt e das funções de login e registrar

--- src/auth/AuthService.js
+++ src/auth/AuthService.js
@@ -1,13 +1,32 @@
+const User = require("./User")
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
+
 class AuthService {
     constructor(repository) {
         this.repository = repository
     }
 
-    register() {
+    register(name, email, password) {
+        const userExists = this.repository.findByEmail(email)
+        if (userExists) throw new Error('This email was already used by another user.')
 
+        const newUser = new User({ name, email, password })
+        newUser.password = bcrypt.hashSync(newUser.password)
+        this.repository.save(newUser)
+        return newUser
     }
 
     login() {
-        
+        const user = this.repository.findByEmail(email)
+        if (!user) throw new Error('User not found.')
+
+        const isSamePassword = bcrypt.compareSync(password, user.password)
+        if (!isSamePassword) throw new Error('Wrong password.')
+
+        const token = jwt.sign({ id: user.id, email: user.email }, 'shh', { expiresIn: '1d' })
+        return { token, user }
     }
 }
+
+module.exports = AuthService
